a4a7e0d2a2a96af3921a1cf56db7ff57062ab8d9
abyad-tif <lapajung50@gmail.com>
2025-10-02 18:03:54 +0800

    perbaikan register

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -71,16 +71,6 @@ export default function LoginPage() {
             </button>
           </div>
         </form>
-
-        <p className="mt-10 text-center text-sm/6 text-gray-400">
-          Not a member?
-          <a
-            href="#"
-            className="font-semibold text-indigo-400 hover:text-indigo-300"
-          >
-            Start a 14 day free trial
-          </a>
-        </p>
       </div>
     </div>
   );
